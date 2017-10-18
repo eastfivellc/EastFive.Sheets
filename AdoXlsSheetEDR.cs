b4e5f1fdf989aac801ae87d3a19c707143d1a82f
Keith Holloway <keith@eastfive.com>
2017-10-18 13:26:39 -0500

    Added xls reader that uses the ExcelDataReader Nuget package.  This was to overcome some limitations in the ODBC implementation.

---
+++ AdoXlsSheetEDR.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace EastFive.Sheets
+{
+    class AdoXlsSheetERF : ISheet
+    {
+        private DataTable dataTable;
+
+        public AdoXlsSheetERF(DataTable dataTable)
+        {
+            this.dataTable = dataTable;
+        }
+
+        public IEnumerable<string[]> ReadRows()
+        {
+            foreach(DataRow row in dataTable.Rows)
+            {
+                yield return Enumerable
+                    .Range(0, dataTable.Columns.Count)
+                    .Select(
+                        columnIndex =>
+                        {
+                            var x = row[columnIndex];
+                            if (typeof(string).IsInstanceOfType(x))
+                                return x.ToString();
+                            if (typeof(DBNull).IsInstanceOfType(x))
+                                return string.Empty;
+                            return x.ToString();
+                        })
+                    .ToArray();
+            }
+        }
+    }
+}
---
+++ AdoXlsWorkbookEDR.cs
@@ -0,0 +1,70 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+
+namespace EastFive.Sheets
+{
+    public class AdoXlsWorkbookERF : IUnderstandSheets
+    {
+        private DataSet data;
+
+        public AdoXlsWorkbookERF()
+        {
+
+        }
+
+        //public AdoXlsWorkbook(OleDbConnection conn, string filename)
+        public AdoXlsWorkbookERF(DataSet data)
+        {
+            this.data = data;
+        }
+
+        public static AdoXlsWorkbook Load(byte [] xlsFile)
+        {
+            using (MemoryStream stream = new MemoryStream(xlsFile))
+            {
+                // Auto-detect format, supports:
+                //  - Binary Excel files (2.0-2003 format; *.xls)
+                //  - OpenXml Excel files (2007 format; *.xlsx)
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    var data = reader.AsDataSet();
+                    return new AdoXlsWorkbook(data);
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            data.Dispose();
+        }
+
+        public TResult ReadCustomValues<TResult>(Func<KeyValuePair<string, string>[], TResult> onResults)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ISheet> ReadSheets()
+        {
+            if (data.Tables == null || data.Tables.Count == 0)
+                yield break;
+
+            foreach(DataTable table in data.Tables)
+            {
+                yield return new AdoXlsSheet(table);
+            }
+        }
+
+        public TResult WriteCustomProperties<TResult>(Func<Action<string, string>, TResult> callback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TResult WriteSheetByRow<TResult>(Func<Action<object[]>, TResult> p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
