28567896251f11cd8d75a658eacc93973a12c741
Scott Turner <turner-scott@tds.net>
2018-01-09 15:12:55 -0600

    extensions to extract fields from csv formatted files

---
+++ CsvSheetExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EastFive.Sheets
+{
+    public static class CsvSheetExtensions
+    {
+        public static IEnumerable<string[]> ReadRows(this Stream stream, string delimiter = ",")
+        {
+            stream.Seek(0, SeekOrigin.Begin);
+            using (var parser = new Microsoft.VisualBasic.FileIO.TextFieldParser(stream)
+            {
+                TextFieldType = Microsoft.VisualBasic.FileIO.FieldType.Delimited,
+                Delimiters = new[] { delimiter }
+            })
+            {
+                while (!parser.EndOfData)
+                {
+                    string[] fields;
+                    try
+                    {
+                        fields = parser.ReadFields();
+                    }
+                    catch (Exception)
+                    {
+                        continue;
+                    }
+                    yield return fields;
+                }
+            }
+        }
+
+        public static TResult GetLegend<TInput,TEnum,TResult>(this TInput[] fields, Dictionary<string, TEnum> map, 
+            Func<TInput, string> getFieldText,
+            Func<Dictionary<TEnum, int>,TResult> onParsed)
+        {
+            var legend = new Dictionary<TEnum, int>();
+            for (var i = 0; i < fields.Length; i++)
+            {
+                if (map.TryGetValue(getFieldText(fields[i]), out TEnum field))
+                    legend.Add(field, i);
+            }
+            return onParsed(legend);
+        }
+
+        public static TResult GetColumn<TEnum,TResult>(this string[] fields, Dictionary<TEnum, int> legend, TEnum column,
+            Func<string,TResult> setFunc, TResult defaultValue = default(TResult))
+        {
+            return legend.TryGetValue(column, out int index) ? setFunc(fields[index]) : defaultValue;
+        }
+    }
+}
