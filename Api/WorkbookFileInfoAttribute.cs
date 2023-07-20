97ff971a26cc0dc4a72f8d9238b43ba5ed2cb3f3
Joshua Wingstrom <josh@eastfive.com>
2023-07-20 07:58:25 -0500

    Added ability to parse csv and xlsx on single API variable

--- Api/WorkbookFileInfoAttribute.cs
+++ Api/WorkbookFileInfoAttribute.cs
@@ -7,16 +7,19 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-using EastFive.Api;
-using EllipticCurve.Utils;
 using Microsoft.AspNetCore.Http;
+
+using EastFive.Api;
+using DocumentFormat.OpenXml.Spreadsheet;
 
 namespace EastFive.Sheets.Api
 {
     public class WorkbookFileInfoAttribute : Attribute,
         IProvideBlobValue
     {
-        public TResult ProvideValue<TResult>(MultipartContentTokenParser valueToBind, Func<object, TResult> onBound, Func<string, TResult> onFailure)
+        public TResult ProvideValue<TResult>(MultipartContentTokenParser valueToBind,
+            Func<object, TResult> onBound,
+            Func<string, TResult> onFailure)
         {
             var raw = valueToBind.ReadStream().ToBytes();
             var stream = new MemoryStream(raw);
@@ -30,22 +33,70 @@ namespace EastFive.Sheets.Api
             return onBound(info);
         }
 
-        public TResult ProvideValue<TResult>(IFormFile valueToBind, Func<object, TResult> onBound, Func<string, TResult> onFailure)
+        public TResult ProvideValue<TResult>(IFormFile valueToBind,
+            Func<object, TResult> onBound,
+            Func<string, TResult> onFailure)
         {
             var raw = valueToBind.OpenReadStream().ToBytes();
-            var stream = new MemoryStream(raw);
-            var workbook = new OpenXmlWorkbook(stream);
-            var sheetUnderstander = (IUnderstandSheets)workbook;
-            var info = new WorkbookFileInfo
+
+            return GetWorkbook(
+                sheetUnderstander =>
+                {
+
+                    var info = new WorkbookFileInfo
+                    {
+                        contentDisposition = new ContentDisposition(valueToBind.ContentDisposition),
+                        contentType = new ContentType(valueToBind.ContentType),
+                        headers = valueToBind.Headers,
+                        length = valueToBind.Length,
+                        workbook = sheetUnderstander,
+                        raw = raw,
+                    };
+                    return onBound(info);
+                },
+                onFailure: onFailure);
+
+            TResult GetWorkbook(
+                Func< IUnderstandSheets, TResult> onBound,
+                Func<string, TResult> onFailure)
             {
-                contentDisposition = new ContentDisposition(valueToBind.ContentDisposition),
-                contentType = new ContentType(valueToBind.ContentType),
-                headers = valueToBind.Headers,
-                length = valueToBind.Length,
-                workbook = sheetUnderstander,
-                raw = raw,
-            };
-            return onBound(info);
+                if (valueToBind.ContentType.Contains("csv", StringComparison.OrdinalIgnoreCase))
+                {
+                    var stream = new MemoryStream(raw);
+                    var workbook = new CsvWorkbook(stream);
+                    var sheetUnderstander = (IUnderstandSheets)workbook;
+                    return onBound(sheetUnderstander);
+                }
+
+                if (IsXlsx())
+                {
+                    var stream = new MemoryStream(raw);
+                    var workbook = new OpenXmlWorkbook(stream);
+                    var sheetUnderstander = (IUnderstandSheets)workbook;
+                    return onBound(sheetUnderstander);
+                }
+
+                return onFailure($"Could not process file of type:`{valueToBind.ContentType}`");
+
+                bool IsXlsx()
+                {
+                    var contentType = valueToBind.ContentType;
+
+                    if (contentType.Contains("openxmlformats", StringComparison.OrdinalIgnoreCase))
+                        if (contentType.Contains("sheet", StringComparison.OrdinalIgnoreCase))
+                            return true;
+
+                    if (contentType.Contains("xlsx", StringComparison.OrdinalIgnoreCase))
+                        return true;
+
+                    if (contentType.Contains("officedocument", StringComparison.OrdinalIgnoreCase))
+                        if (contentType.Contains("sheet", StringComparison.OrdinalIgnoreCase))
+                            return true;
+
+
+                    return false;
+                }
+            }
         }
     }
 }
---
+++ CsvWorkbook.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using CsvHelper;
+using EastFive.Extensions;
+
+namespace EastFive.Sheets
+{
+    public class CsvWorkbook : IUnderstandSheets
+    {
+        private readonly CsvSheet sheet;
+
+        public CsvWorkbook(Stream stream)
+        {
+            this.sheet = new CsvSheet(stream);
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public TResult ReadCustomValues<TResult>(
+            Func<KeyValuePair<string, string>[], TResult> onResults)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ISheet> ReadSheets()
+        {
+            return sheet.AsEnumerable();
+        }
+
+        public TResult WriteCustomProperties<TResult>(Func<Action<string, string>, TResult> callback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TResult WriteSheetByRow<TResult>(
+            Func<Action<object[]>, TResult> p,
+            string sheetName = "Sheet1")
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
